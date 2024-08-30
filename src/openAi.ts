b0ea6210dadc6cdfb9d1bc33bfbddd1811a638a7
Jz Jektvik <29360158+jektvik@users.noreply.github.com>
2024-08-30 14:12:40 +0200

    gpt-4o-mini

--- src/openAi.ts
+++ src/openAi.ts
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 
 
 export const MAX_OPEN_AI_QUERY_LENGTH = 20000;
-export const MODEL_NAME = "text-davinci-003";
+export const MODEL_NAME = "gpt-4o-mini";
 export const TEMPERATURE = 0.5;
 export const MAX_TOKENS = 512;
 
